73c4cb45f5893711a448adbfa26495548120914c
paranoid_m42 <cemlur125@gmai.com>
2024-06-07 15:47:00 +0300

    42

--- CPP07/ex02/Array.hpp
+++ CPP07/ex02/Array.hpp
@@ -1,76 +1,70 @@
-// Array.h
-#ifndef ARRAY_H
-#define ARRAY_H
+#pragma once
 
 #include <exception>
 
 template <typename T>
 class Array {
-private:
-    T* data;
-    unsigned int n;
+    private:
+        T* data;
+        unsigned int n;
 
-public:
-    // Default constructor
-    Array() : data(NULL), n(0) {}
+    public:
+        // Default constructor
+        Array() : data(NULL), n(0) {}
 
-    // Constructor with size parameter
-    Array(unsigned int size) : n(size)
-    {
-        data = new T[n];
-    }
-
-    // Copy constructor
-    Array(const Array& other) : n(other.n) 
-    {
-        data = new T[n];
-        for (unsigned int i = 0; i < n; ++i) {
-            data[i] = other.data[i];
-        }
-    }
-
-    // Assignment operator
-    Array& operator=(const Array& other) {
-        if (this != &other) {
-            delete[] data;
-            n = other.n;
+        // Constructor with size parameter
+        Array(unsigned int size) : n(size)
+        {
             data = new T[n];
-            for (unsigned int i = 0; i < n; ++i) {
+        }
+
+        // Copy constructor
+        Array(const Array& other) : n(other.n) 
+        {
+            data = new T[n];
+            for (unsigned int i = 0; i < n; ++i) 
                 data[i] = other.data[i];
+        }
+
+        // Assignment operator
+        Array& operator=(const Array& other) {
+            if (this != &other) 
+            {
+                delete[] data;
+                n = other.n;
+                data = new T[n];
+                for (unsigned int i = 0; i < n; ++i) {
+                    data[i] = other.data[i];
+                }
             }
+            return *this;
         }
-        return *this;
-    }
 
-    // Destructor
-    ~Array() 
-    {
-        delete[] data;
-    }
-
-    // Subscript operator
-    T& operator[](unsigned int index) 
-    {
-        if (index >= n) {
-            throw std::out_of_range("Index out of bounds");
+        // Destructor
+        ~Array() 
+        {
+            delete[] data;
         }
-        return data[index];
-    }
 
-    // Const subscript operator
-    const T& operator[](unsigned int index) const 
-    {
-        if (index >= n) {
-            throw std::out_of_range("Index out of bounds");
+        // Subscript operator
+        T& operator[](unsigned int index) 
+        {
+            if (index >= n)
+                throw std::out_of_range("Index out of bounds");
+    
+            return data[index];
         }
-        return data[index];
-    }
 
-    // Size function
-    unsigned int size() const 
-    {
-        return n;
-    }
+        // Const subscript operator
+        const T& operator[](unsigned int index) const 
+        {
+            if (index >= n) 
+                throw std::out_of_range("Index out of bounds");
+            
+            return data[index];
+        }
+
+        // Size function
+        unsigned int size() const {  return n; }
 };
 
-#endif // ARRAY_H
