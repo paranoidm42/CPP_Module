d2a5a1c84cff8bf29ca4caef9d70ac9946dc4036
paranoid_m42 <cemlur125@gmai.com>
2024-06-25 00:24:11 +0300

    42

---
+++ CPP08/ex02/MutantStack.hpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <stack>
+#include <deque>
+
+template<typename T>
+class MutantStack : public std::stack<T>
+{
+    public:
+        typedef typename std::deque<T>::iterator iterator;
+
+        MutantStack() : std::stack<T>() {}
+        MutantStack(const MutantStack<T>& src) : std::stack<T>(src) {}
+        ~MutantStack() {}
+        
+        MutantStack<T>& operator=(const MutantStack<T>& rhs) 
+        {
+            if (this != &rhs) {
+                std::stack<T>::operator=(rhs);
+            }
+            return *this;
+        }
+
+        iterator begin() 
+        {
+            return this->c.begin();
+        }
+
+        iterator end() {
+            return this->c.end();
+        }
+};
