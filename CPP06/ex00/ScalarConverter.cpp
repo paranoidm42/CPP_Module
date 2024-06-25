c42fe4e4e35f3c6aeb33abbf4a887ad4de55ce7b
Cemil Cur <ccur@k2m09s01.42kocaeli.com.tr>
2024-06-25 13:31:43 +0300

    42

--- CPP06/ex00/ScalarConverter.cpp
+++ CPP06/ex00/ScalarConverter.cpp
@@ -26,14 +26,13 @@ ScalarConverter::~ScalarConverter()
 }
 
 
-bool is_pseudo(const std::string input)
+static bool is_pseudo(const std::string input)
 {
     if (input == "nanf" || input == "-inff" || input == "+inff" || input == "nan" || \
-		input == "-inf" || input == "+inf" || input == "inf")
-		return true;
-	return false;
+        input == "-inf" || input == "+inf" || input == "inf" || input == "inff" || input == "nanff")
+        return true;
+    return false;
 }
-
 
 void	printpseudo(std::string input)
 {
@@ -42,7 +41,7 @@ void	printpseudo(std::string input)
 		std::cout << "char: " << "impossible" << "\n";
 		std::cout << "int: " << "impossible" << "\n";
 		std::cout << "float: " << ((input[input.size() - 1] == 'f') ? input : (input + 'f')) << std::endl;
-		std::cout << "input: " << input << "\n";
+		std::cout << "double: " << input << "\n";
 	}
 }
 
@@ -69,15 +68,17 @@ void ScalarConverter::toChar(std::string input)
 {
     std::cout << "char: " ;
     try
-    {
-        if(input.length() == 1 && !std::isdigit(input[0]) && std::isprint(input[0]))
-            std::cout << "'" <<static_cast<char>(input[0]) << "'" << std::endl;
-        else if(std::isprint(static_cast<char>(std::atoi(input.c_str()))))
-        	std::cout << "'" << static_cast<char>(std::atoi(input.c_str())) << "'" << std::endl;
-        else
-            std::cout << "Non displayable" << std::endl;
+	{
+		if(std::atoi(input.c_str()) > 255)
+			std::cout << "impossible" << std::endl;
+		else if(input.length() == 1 && !std::isdigit(input[0]) && std::isprint(input[0]))
+			std::cout << "'" <<static_cast<char>(input[0]) << "'" << std::endl;
+		else if(std::isprint(static_cast<char>(std::atoi(input.c_str()))))
+			std::cout << "'" << static_cast<char>(std::atoi(input.c_str())) << "'" << std::endl;
+		else
+			std::cout << "Non displayable" << std::endl;
 
-    }
+	}
     catch(const std::exception& e)
     {
         std::cerr << "impossible" << std::endl;
@@ -88,19 +89,19 @@ void ScalarConverter::toChar(std::string input)
 void	ScalarConverter::toInt(std::string input)
 {
 	std::cout << "int: ";
-	try
-	{
-		if(std::isdigit(input[0]))
-			std::cout << std::atoi(input.c_str());
-		else if (input.length() == 1 && std::isprint(input[0]))
-			std::cout << static_cast<int>(input[0]);
-		else
-			std::cout << std::atoi(input.c_str());
-		std::cout << std::endl;
+	try {
+		int num = std::stoi(input);
+		std::cout << num << std::endl;
 
-	}
-	catch (std::exception &e)
+	} 
+	catch (const std::invalid_argument& ia) 
 	{
+		if (input.length() == 1 && std::isprint(input[0]))
+			std::cout << static_cast<int>(input[0]) << std::endl;
+		else
+			std::cout << "impossible" << std::endl;
+	} 
+	catch (const std::out_of_range& oor) {
 		std::cout << "impossible" << std::endl;
 	}
 }
@@ -111,11 +112,11 @@ void ScalarConverter::toFloat(std::string input)
 	try
 	{
 		if(std::isdigit(input[0]))
-			std::cout << std::fixed << std::setprecision(1) << static_cast<float>(std::atof(input.c_str())) << "f";
+			std::cout << std::fixed << std::setprecision(1) << static_cast<float>(std::stof(input)) << "f";
 		else if (input.length() == 1 && std::isprint(input[0]))
 			std::cout << std::fixed << std::setprecision(1) << static_cast<float>(input[0])<< "f";
 		else
-			std::cout << std::fixed << std::setprecision(1) << static_cast<float>(std::atof(input.c_str())) << "f";
+			std::cout << std::fixed << std::setprecision(1) << static_cast<float>(std::stof(input)) << "f";
 		std::cout << std::endl;
 	}
 	catch (std::exception &e)
@@ -130,11 +131,11 @@ void ScalarConverter::toDouble(std::string input)
 	try
 	{
 		if(std::isdigit(input[0]))
-			std::cout << std::fixed << std::setprecision(1) << static_cast<double>(std::atof(input.c_str()));
+			std::cout << std::fixed << std::setprecision(1) << static_cast<double>(std::strtod(input.c_str(), nullptr));
 		else if (input.length() == 1 && std::isprint(input[0]))
 			std::cout << std::fixed << std::setprecision(1) << static_cast<double>(input[0]);
 		else
-			std::cout << std::fixed << std::setprecision(1) << static_cast<double>(std::atof(input.c_str()));
+			std::cout << std::fixed << std::setprecision(1) << static_cast<double>(std::strtod(input.c_str(), nullptr));
 
 
 		std::cout << std::endl;
@@ -143,4 +144,5 @@ void ScalarConverter::toDouble(std::string input)
 	{
 		std::cout << "impossible" << std::endl;
 	}
+	
 }
--- CPP06/ex00/main.cpp
+++ CPP06/ex00/main.cpp
@@ -1,19 +1,56 @@
 #include "ScalarConverter.hpp"
 
+
+
+bool is_pseudo(const std::string input)
+{
+    if (input == "nanf" || input == "-inff" || input == "+inff" || input == "nan" || \
+        input == "-inf" || input == "+inf" || input == "inf" || input == "inff" || input == "nanff")
+        return true;
+    return false;
+}
+
+bool isNumeric(std::string input)
+{
+    int sign = 0;
+
+    if(input[0] == '-' || input[0] == '+')
+        sign++;
+    for ( int i = sign; i < (int)input.length(); i++)
+    {
+        if(!std::isdigit(input[i]))
+        {
+            if((input[i] == '.' && input[input.length() - 1] == 'f') || input[i] == '.' || input[i] == 'f')
+            {
+                for(int x = ++i; x < (int)input.length() - 1; x++)
+                {
+                    if(!std::isdigit(input[x]))
+                        return false;
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+    return true;
+}
+
+
+
 int main(int ac, char **av)
 {
-    try
+
+    if (ac == 2)
     {
-        if (ac == 2)
-        {
-            ScalarConverter::convert(av[1]);   
-            return 1;
-        }   
-    }
-    catch(std::exception& e)
-    {
-		std::cout << "impossible" << std::endl;
-    } 
+        std::string input = av[1];
+
+        if(isNumeric(input) || input.length() == 1 || is_pseudo(input))
+            ScalarConverter::convert(av[1]);
+        else
+            std::cout << "Undefined arg!!" << std::endl;
+        return 0;
+    }   
+
     std::cout << "Error: This program accepts only 1 argument!" << std::endl;
     return 0;
 }
